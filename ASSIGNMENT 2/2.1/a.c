ece00d881853fd439e7421db18bb6bf4d3a3af6d
moksh4102 <mokshjoshee@gmail.com>
2024-01-26 15:29:50 +0530

    add folder

---
+++ ASSIGNMENT 2/2.1/a.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("your name : MOKSH JOSHI");
+    printf("your birth date: 04/10/2002");
+    printf("your age:21");
+    printf("your address:68 sai darshan society");
+}
